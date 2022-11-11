4e502b57f0385f4df478137eca630cff7c9469f1
vldmkr <vladmakaryan@gmail.com>
2022-11-11 19:20:01 +0200

    Add documentation generation

--- src/Aptos.ts
+++ src/Aptos.ts
@@ -27,22 +27,62 @@ interface AddressData {
   address: string;
 }
 
+/**
+ * Aptos API
+ *
+ * @param transport a transport for sending commands to a device
+ * @param scrambleKey a scramble key
+ *
+ * @example
+ * import Aptos from "hw-app-aptos";
+ * const aptos = new Aptos(transport);
+ */
 export default class Aptos {
   readonly transport: Transport;
 
-  constructor(transport: Transport, scrambleKey = "aptos") {
+  constructor(
+    transport: Transport,
+    // the type annotation is needed for doc generator
+    // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+    scrambleKey: string = "aptos"
+  ) {
     this.transport = transport;
     this.transport.decorateAppAPIMethods(this, ["getVersion", "getAddress"], scrambleKey);
   }
 
+  /**
+   * Get application version.
+   *
+   * @returns an object with the version field
+   *
+   * @example
+   * aptos.getVersion().then(r => r.version)
+   */
   async getVersion(): Promise<AppConfig> {
     const [major, minor, patch] = await this.sendToDevice(INS.GET_VERSION, P1_NON_CONFIRM, P2_LAST, Buffer.alloc(0));
     return {
       version: `${major}.${minor}.${patch}`,
     };
   }
-
-  async getAddress(path: string, display = false): Promise<AddressData> {
+  /**
+   * Get Aptos address (public key) for a BIP32 path.
+   *
+   * Because Aptos uses Ed25519 keypairs, as per SLIP-0010
+   * all derivation-path indexes will be promoted to hardened indexes.
+   *
+   * @param path a BIP32 path
+   * @param display flag to show display
+   * @returns an object with publicKey, chainCode, address fields
+   *
+   * @example
+   * aptos.getAddress("m/44'/637'/1'/0'/0'").then(r => r.address)
+   */
+  async getAddress(
+    path: string,
+    // the type annotation is needed for doc generator
+    // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+    display: boolean = false
+  ): Promise<AddressData> {
     const pathBuffer = this.pathToBuffer(path);
     const responseBuffer = await this.sendToDevice(INS.GET_PUBLIC_KEY, display ? P1_CONFIRM : P1_NON_CONFIRM, P2_LAST, pathBuffer);
 
@@ -61,6 +101,17 @@ export default class Aptos {
     };
   }
 
+  /**
+   * Sign an Aptos transaction.
+   *
+   * @param path a BIP32 path
+   * @param txBuffer serialized transaction
+   *
+   * @returns an object with the signature field
+   *
+   * @example
+   * aptos.signTransaction("m/44'/637'/1'/0'/0'", txBuffer).then(r => r.signature)
+   */
   async signTransaction(path: string, txBuffer: Buffer): Promise<{ signature: Buffer }> {
     const pathBuffer = this.pathToBuffer(path);
     await this.sendToDevice(INS.SIGN_TX, P1_START, P2_MORE, pathBuffer);
